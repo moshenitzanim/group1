fa6ff4e92d707b31f3554d143f150580f3a079cd
yehonatan <yehonatanbas@gmail.com>
2024-03-07 18:26:06 +0200

    Wrote the function "print_funny_story"

--- Basel.py
+++ Basel.py
@@ -1,2 +1,2 @@
 def print_funny_story():
-    print("Its Yehonatan Basel, I didn't notice that there were some black people in front of me and i said niggas, they laughed.")
+    print("Its Yehonatan Basel")
